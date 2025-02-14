e81efd6a42af7473fdb115e96b024298eb953d9a
John Long <a@88k.us>
2025-02-14 10:56:55 -0600

    openssl-file-encryption: initial commit

---
+++ openssl-file-encryption/README.md
@@ -0,0 +1,18 @@
+# Exercise: Deterministic File Encryption using OpenSSL
+
+This exercise shows how files can be encrypted and decrypted deterministically using OpenSSL.
+
+## Usage
+
+```bash
+# To print the usage information
+./script.sh
+
+# To encrypt one or more files
+./script.sh encrypt example.txt
+./script.sh encrypt example.txt example2.txt
+
+# To decrypt one or more files
+./script.sh decrypt example.txt.enc
+./script.sh decrypt example.txt.enc example2.txt.enc
+```
---
+++ openssl-file-encryption/script.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+preflight() {
+  # Error out on command failure
+  set -o errexit
+
+  # Error out on pipefail
+  set -o pipefail
+
+  # Error out on undefined variables
+  set -o nounset
+
+  # Load in environment variable file (if it exists)
+  [ -f .env ] && { set -a; source .env; set +a; }
+
+  # Ensure required environment variables are set
+  require_env "SALT"
+  require_env "PASS"
+
+  if [ "$#" -lt 2 ]; then
+	err "Not enough arguments"
+    echo "Usage: ./script.sh [encrypt|decrypt] <...files>"
+	echo ""
+	echo "Encrypts or decrypts files using AES-256-CBC with PBKDF2 and a salt"
+	echo "The SALT and PASS environment variables must be set (or provided in .env)"
+	echo ""
+	echo "Example: ./script.sh encrypt file1.txt file2.txt"
+	echo "Example: ./script.sh decrypt file1.txt.enc file2.txt.enc"
+    exit 1
+  fi
+
+  # Set the image to use
+  IMAGE="alpine/openssl:3.3.2"
+
+  # Set the command to use
+  CMD="docker run --rm \
+  	--env SALT --env PASS \
+	-v .:/data -w /data \
+	${IMAGE} \
+	aes-256-cbc -pbkdf2 -iter 20000 -salt -md sha256 -a -S ${SALT} -pass env:PASS"
+}
+
+err() { echo "$@" 1>&2; }
+errexit() { err "$@"; exit 1; }
+require_env() { [ ! -z "$1" ] || errexit "Env var $1 is required, but not set"; }
+
+encrypt() {
+  for file in "$@"; do
+	[ -f "$file" ] || errexit "File not found: $file"
+	$CMD -e -in "$file" -out "${file}.enc"
+  done
+}
+
+decrypt() {
+  for file in "$@"; do
+	[ -f "$file" ] || errexit "File not found: $file"
+	$CMD -d -in "$file" -out "${file%.enc}"
+  done
+}
+
+main() {
+  preflight "$@"
+
+  case "$1" in
+    encrypt) encrypt "${@:2}" ;;
+    decrypt) decrypt "${@:2}" ;;
+    *) errexit "Invalid command: $1"
+  esac
+}
+
+main "$@"
