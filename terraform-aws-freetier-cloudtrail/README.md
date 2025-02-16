a242a7264dff7161cf080aaa05d05c976f39db8e
John Long <a@88k.us>
2025-02-15 18:03:30 -0600

    terraform-aws-freetier-cloudtrail: initial commit

---
+++ terraform-aws-freetier-cloudtrail/README.md
@@ -0,0 +1,17 @@
+# Exercise: Free-tier Organization CloudTrail using Terraform
+
+## Overview
+
+This exercise creates a free-tier-eligible CloudTrail to monitor organization activity. It may be useful to audit permission issues within the organization.
+
+## Instructions
+
+1. Clone the repository
+2. Copy `terraform.dist.tfvars` to `terraform.tfvars` and set the variables
+3. Run `terraform init`
+4. Run `terraform plan`
+5. Run `terraform apply`
+
+## Links
+
+- [AWS Free Tier](https://aws.amazon.com/free/)
---
+++ terraform-aws-freetier-cloudtrail/main.tf
@@ -0,0 +1,191 @@
+data "aws_caller_identity" "current" {}
+data "aws_partition" "current" {}
+data "aws_region" "current" {}
+data "aws_organizations_organization" "current" {}
+
+resource "aws_cloudtrail" "org_cloudtrail" {
+  name                          = "freetier-org-cloudtrail"
+  s3_bucket_name                = aws_s3_bucket.bucket.id
+  cloud_watch_logs_group_arn    = "${aws_cloudwatch_log_group.log_group.arn}:*"
+  cloud_watch_logs_role_arn     = aws_iam_role.cloud_watch_logs_role.arn
+  include_global_service_events = true
+  is_organization_trail         = true
+  is_multi_region_trail         = true
+
+  event_selector {
+    read_write_type           = "All"
+    include_management_events = true
+  }
+
+  depends_on = [aws_s3_bucket_policy.bucket_policy]
+}
+
+## Bucket
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = format(
+    "%s-freetier-org-cloudtrail-bucket",
+    data.aws_organizations_organization.current.master_account_name
+  )
+  force_destroy = true
+}
+
+data "aws_iam_policy_document" "bucket_policy" {
+  statement {
+    sid    = "S3GetBucketAcl"
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+
+    actions   = ["s3:GetBucketAcl"]
+    resources = [aws_s3_bucket.bucket.arn]
+
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceArn"
+      values = [
+        format(
+          "arn:%s:cloudtrail:%s:%s:trail/freetier-org-cloudtrail",
+          data.aws_partition.current.partition,
+          data.aws_region.current.name,
+          data.aws_organizations_organization.current.master_account_id
+        )
+      ]
+    }
+  }
+
+  statement {
+    sid    = "S3PutObject"
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+
+    actions = ["s3:PutObject"]
+    resources = [
+      format(
+        "%s/AWSLogs/%s/*",
+        aws_s3_bucket.bucket.arn,
+        data.aws_organizations_organization.current.master_account_id
+      )
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "s3:x-amz-acl"
+      values   = ["bucket-owner-full-control"]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceArn"
+      values = [
+        format(
+          "arn:%s:cloudtrail:%s:%s:trail/freetier-org-cloudtrail",
+          data.aws_partition.current.partition,
+          data.aws_region.current.name,
+          data.aws_organizations_organization.current.master_account_id
+        )
+      ]
+    }
+  }
+
+  statement {
+    sid    = "OrgS3PutObject"
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+    actions = ["s3:PutObject"]
+    resources = [
+      format(
+        "%s/AWSLogs/%s/*",
+        aws_s3_bucket.bucket.arn,
+        data.aws_organizations_organization.current.id
+      )
+    ]
+    condition {
+      test     = "StringEquals"
+      variable = "s3:x-amz-acl"
+      values   = ["bucket-owner-full-control"]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceArn"
+      values = [
+        format(
+          "arn:%s:cloudtrail:%s:%s:trail/freetier-org-cloudtrail",
+          data.aws_partition.current.partition,
+          data.aws_region.current.name,
+          data.aws_organizations_organization.current.master_account_id
+        )
+      ]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.bucket.id
+  policy = data.aws_iam_policy_document.bucket_policy.json
+}
+
+## CloudWatch Logs
+
+resource "aws_cloudwatch_log_group" "log_group" {
+  name = "freetier-org-cloudtrail-log-group"
+}
+
+data "aws_iam_policy_document" "cloud_watch_logs_role_assume" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    effect  = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "cloud_watch_logs_role_policy" {
+  statement {
+    sid = "CreateLogStream"
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+    effect = "Allow"
+    resources = [
+      format(
+        "arn:%s:logs:%s:%s:log-group:%s:log-stream:%s_CloudTrail_%s*",
+        data.aws_partition.current.partition,
+        data.aws_region.current.name,
+        data.aws_caller_identity.current.account_id,
+        aws_cloudwatch_log_group.log_group.name,
+        data.aws_caller_identity.current.account_id,
+        data.aws_region.current.name
+      ),
+      format(
+        "arn:%s:logs:%s:%s:log-group:%s:log-stream:%s_*",
+        data.aws_partition.current.partition,
+        data.aws_region.current.name,
+        data.aws_caller_identity.current.account_id,
+        aws_cloudwatch_log_group.log_group.name,
+        data.aws_organizations_organization.current.id
+      )
+    ]
+  }
+}
+
+resource "aws_iam_role" "cloud_watch_logs_role" {
+  name               = "freetier-org-cloudtrail-cloud-watch-logs-role"
+  assume_role_policy = data.aws_iam_policy_document.cloud_watch_logs_role_assume.json
+  inline_policy {
+    name   = "cloud_watch_logs_role_policy"
+    policy = data.aws_iam_policy_document.cloud_watch_logs_role_policy.json
+  }
+}
