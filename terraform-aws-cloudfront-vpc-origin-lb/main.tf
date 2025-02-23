aafd73d3686e829e6b55d4902cc9223f16d4394e
John Long <a@88k.us>
2025-02-23 15:24:29 -0600

    cleanup

--- terraform-aws-cloudfront-vpc-origin-lb/main.tf
+++ terraform-aws-cloudfront-vpc-origin-lb/main.tf
@@ -36,8 +36,7 @@ module "vpc" {
   azs  = slice(data.aws_availability_zones.available.names, 0, 3)
   cidr = "10.0.0.0/16"
 
-  public_subnets = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24"]
-
+  public_subnets  = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24"]
   private_subnets = ["10.0.21.0/24", "10.0.22.0/24", "10.0.23.0/24"]
 }
 
