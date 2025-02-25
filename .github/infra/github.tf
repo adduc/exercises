af2e265a9e98aebb38cb019c31abc1250d072d1e
John Long <a@88k.us>
2025-02-25 10:12:21 -0600

    .github/infra: disable merge commit and rebase merges, force squash merges

--- .github/infra/github.tf
+++ .github/infra/github.tf
@@ -16,5 +16,9 @@ import {
 resource "github_repository" "repo" {
   name = "exercises"
 
+  # PRs
+  allow_merge_commit     = false
+  allow_rebase_merge     = false
+  allow_squash_merge     = true
   delete_branch_on_merge = true
 }
