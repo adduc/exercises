75c30473751d4830437d703d564f5405058cb166
John Long <a@88k.us>
2025-02-28 14:14:35 -0600

    golang-rest-api: move persistence from model to repository

--- golang-rest-api/models/user.go
+++ golang-rest-api/models/user.go
@@ -1,11 +1,7 @@
 package models
 
 import (
-	"log"
 	"time"
-
-	"github.com/adduc/exercises/golang-rest-api/db"
-	"github.com/adduc/exercises/golang-rest-api/errors"
 )
 
 type User struct {
@@ -14,28 +10,4 @@ type User struct {
 	Password  string
 	CreatedAt time.Time
 	UpdatedAt time.Time
-}
-
-func (u *User) List() ([]User, error) {
-	var users []User
-	result := db.GetDB().Find(&users)
-	return users, result.Error
-}
-
-func (u *User) Create() error {
-	var conn = db.GetDB()
-
-	// check if email already exists
-	var user User
-	if result := conn.Where("email = ?", u.Email).Limit(1).Find(&user); result.Error != nil {
-		log.Println("Error retrieving user", result.Error)
-		return result.Error
-	}
-
-	if user.ID != 0 {
-		return &errors.DuplicateEmail{}
-	}
-
-	result := conn.Create(u)
-	return result.Error
 }
