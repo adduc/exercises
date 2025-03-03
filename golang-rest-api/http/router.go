3eed5b8769a73c8245854e7884a9e24401a4cb15
John Long <a@88k.us>
2025-03-03 13:13:19 -0600

    golang-rest-api: continue to refactor directory structure

--- golang-rest-api/http/router.go
+++ golang-rest-api/http/router.go
@@ -1,12 +1,21 @@
-package server
+package http
 
 import (
+	"html/template"
+
 	"github.com/adduc/exercises/golang-rest-api/http/controllers"
+	"github.com/adduc/exercises/golang-rest-api/http/resources"
 	"github.com/gin-gonic/gin"
 )
 
 func NewRouter() *gin.Engine {
 	router := gin.Default()
+
+	templ := template.Must(template.New("").ParseFS(resources.F, "templates/*.html"))
+	router.SetHTMLTemplate(templ)
+
+	landing := &controllers.LandingController{}
+	router.GET("/", landing.Landing)
 
 	health := &controllers.HealthController{}
 	router.GET("/health", health.HealthCheck)
