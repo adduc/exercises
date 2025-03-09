254674cff42a9b948bfb49897882e4ed2a5c462f
John Long <a@88k.us>
2025-03-08 18:52:19 -0600

    golang-single-file-api: continue to iterate
    session creation on registration/login is now functional. I'm not a big fan of
    the global state, and would prefer some way to pass substitute implementions
    instead of relying on modifying global state in tests

--- golang-single-file-api/main.go
+++ golang-single-file-api/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"crypto/rand"
 	"embed"
+	"encoding/base64"
 	"fmt"
 	"html/template"
+	"net/http"
+	"time"
 
 	"github.com/caarlos0/env/v8"
 	"github.com/gin-gonic/gin"
 	_ "github.com/joho/godotenv/autoload"
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
@@ -104,10 +109,27 @@ type User struct {
 	Password string
 }
 
+func (u *User) SetPassword(password string) error {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+
+	if err != nil {
+		return err
+	}
+
+	u.Password = string(hashedPassword)
+	return nil
+}
+
+func (u *User) CheckPassword(password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password))
+	return err == nil
+}
+
 type Session struct {
-	ID     int    `gorm:"primaryKey"`
-	UserID int    `gorm:"index"`
-	Token  string `gorm:"unique"`
+	ID        int       `gorm:"primaryKey"`
+	UserID    int       `gorm:"index"`
+	Token     string    `gorm:"unique"`
+	ExpiresAt time.Time `gorm:"notNull"`
 }
 
 // persistence
@@ -121,15 +143,31 @@ func init() {
 }
 
 type UserRepository interface {
+	CreateSession(session *Session) error
+	CreateUser(user *User) error
 	GetUserByID(id int) (*User, error)
+	GetUserByUsername(username string) (*User, error)
 }
 
 type UserDBRepository struct {
 	db gorm.DB
 }
 
+func (r *UserDBRepository) CreateSession(session *Session) error {
+	return r.db.Create(session).Error
+}
+
+func (r *UserDBRepository) CreateUser(user *User) error {
+	return r.db.Create(user).Error
+}
+
 func (r *UserDBRepository) GetUserByID(id int) (user *User, _ error) {
 	result := r.db.Where("id = ?", id).Limit(1).Find(&user)
+	return handleSingleDBResult(user, result)
+}
+
+func (r *UserDBRepository) GetUserByUsername(username string) (user *User, _ error) {
+	result := r.db.Where("username = ?", username).Limit(1).Find(&user)
 	return handleSingleDBResult(user, result)
 }
 
@@ -143,6 +181,51 @@ func handleSingleDBResult[T any](entity *T, result *gorm.DB) (*T, error) {
 	}
 
 	return entity, nil
+}
+
+// session
+
+var SessionManager SessionManagerInterface
+
+func init() {
+	SessionManager = &SessionDBManager{db: DBs.Default}
+}
+
+type SessionManagerInterface interface {
+	CreateSession(user *User) (*Session, error)
+	// ValidateSession(token string) (*Session, error)
+	// InvalidateSession(token string) error
+}
+
+type SessionDBManager struct {
+	db gorm.DB
+}
+
+func (m *SessionDBManager) CreateSession(user *User) (*Session, error) {
+	session := &Session{
+		UserID:    user.ID,
+		Token:     generateSessionToken(),
+		ExpiresAt: time.Now().Add(time.Hour * 24 * 30),
+	}
+
+	if err := m.db.Create(session).Error; err != nil {
+		return nil, err
+	}
+
+	return session, nil
+}
+
+func WriteSessionCookie(c *gin.Context, session *Session) {
+	expiresAt := int(time.Until(session.ExpiresAt).Seconds())
+	c.SetCookie("session_token", session.Token, expiresAt, "/", "", false, true)
+}
+
+func generateSessionToken() string {
+	// generate 128-bit random token to use as session token
+	// @see https://owasp.org/www-community/vulnerabilities/Insufficient_Session-ID_Length
+	token := make([]byte, 16)
+	rand.Read(token)
+	return base64.StdEncoding.EncodeToString(token)
 }
 
 // landing routes
@@ -176,34 +259,83 @@ func Register(c *gin.Context) {
 	c.HTML(200, "register.html", gin.H{})
 }
 
+type RegisterFormInput struct {
+	Username        string `form:"username" binding:"required,min=3,max=24"`
+	Password        string `form:"password" binding:"required,min=8,max=128"`
+	ConfirmPassword string `form:"confirm_password" binding:"required,eqfield=Password"`
+}
+
 func RegisterPost(c *gin.Context) {
+	var input RegisterFormInput
+	if err := c.ShouldBind(&input); err != nil {
+		c.HTML(400, "register.html", gin.H{"error": "Invalid input"})
+		return
+	}
 
-	user, err := Repos.User.GetUserByID(1)
+	existingUser, err := Repos.User.GetUserByUsername(input.Username)
 	if err != nil {
-		c.String(500, "Error retrieving user: %v", err)
+		c.HTML(500, "register.html", gin.H{"error": "Failed to check for existing user"})
+		return
+	} else if existingUser != nil {
+		c.HTML(400, "register.html", gin.H{"error": "Username already taken"})
 		return
 	}
-	if user == nil {
-		c.String(404, "User not found")
+
+	user := &User{Username: input.Username}
+
+	if err := user.SetPassword(input.Password); err != nil {
+		c.HTML(500, "register.html", gin.H{"error": "Failed to set password"})
 		return
 	}
-	c.String(200, "User found: %v", user.Username)
 
-	// Handle registration
-	// user := User{
-	// 	Username: c.PostForm("username"),
-	// 	Password: c.PostForm("password"),
-	// }
+	if err := Repos.User.CreateUser(user); err != nil {
+		c.HTML(500, "register.html", gin.H{"error": "Failed to create user"})
+		return
+	}
 
-	// db.Create(&user)
+	session, err := SessionManager.CreateSession(user)
+	if err != nil {
+		c.HTML(500, "register.html", gin.H{"error": "Failed to create session"})
+		return
+	}
+
+	WriteSessionCookie(c, session)
+	c.Redirect(http.StatusFound, "/dashboard")
 }
 
 func Login(c *gin.Context) {
 	c.HTML(200, "login.html", gin.H{})
 }
 
+type LoginFormInput struct {
+	Username string `form:"username" binding:"required,min=3,max=24"`
+	Password string `form:"password" binding:"required,min=8,max=128"`
+}
+
 func LoginPost(c *gin.Context) {
-	// Handle login
+	var input LoginFormInput
+	if err := c.ShouldBind(&input); err != nil {
+		c.HTML(400, "login.html", gin.H{"error": "Invalid input"})
+		return
+	}
+
+	user, err := Repos.User.GetUserByUsername(input.Username)
+	if err != nil {
+		c.HTML(500, "login.html", gin.H{"error": "Failed to check for existing user"})
+		return
+	} else if user == nil || user.Username != input.Username {
+		c.HTML(400, "login.html", gin.H{"error": "Invalid username or password"})
+		return
+	}
+
+	session, err := SessionManager.CreateSession(user)
+	if err != nil {
+		c.HTML(500, "register.html", gin.H{"error": "Failed to create session"})
+		return
+	}
+
+	WriteSessionCookie(c, session)
+	c.Redirect(http.StatusFound, "/dashboard")
 }
 
 func Logout(c *gin.Context) {
