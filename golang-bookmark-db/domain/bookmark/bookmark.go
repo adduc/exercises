e3c3c25366b10686e983e4bbbf106249afa748ad
John Long <a@88k.us>
2025-03-10 09:32:45 -0500

    pause

--- golang-bookmark-db/domain/bookmark/bookmark.go
+++ golang-bookmark-db/domain/bookmark/bookmark.go
@@ -1,4 +1,15 @@
 package bookmark
 
+import "time"
+
 type Bookmark struct {
+	ID        uint `gorm:"primarykey"`
+	UrlID     uint
+	UserID    uint
+	Note      *string `gorm:"default:null"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+
+	Url  Url  `gorm:"foreignKey:UrlID"`
+	User User `gorm:"foreignKey:UserID"`
 }
