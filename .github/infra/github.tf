75647cd8314e0638abea36b6d60cace831001a07
John S Long <john@johnsdomain.com>
2025-03-10 09:50:59 -0500

    .github/infra: require pull requests for all changes to main branch (#11)

--- .github/infra/github.tf
+++ .github/infra/github.tf
@@ -22,3 +22,17 @@ resource "github_repository" "repo" {
   allow_squash_merge     = true
   delete_branch_on_merge = true
 }
+
+resource "github_branch_default" "default" {
+  repository = github_repository.repo.name
+  branch     = "main"
+}
+
+resource "github_branch_protection" "default" {
+  repository_id = github_repository.repo.id
+  pattern       = github_branch_default.default.branch
+
+  required_pull_request_reviews {
+    required_approving_review_count = 0
+  }
+}
