353a2a583647ba1b02f14eeea54b7e058ab5449d
John S Long <john@johnsdomain.com>
2025-04-03 16:36:25 -0500

    kubernetes/gitlab: initial commit  (#59)
    * kubernetes/gitlab: initial commit
    
    * Flesh out
    
    * Add required_version
    
    * readme: clarify deployment method in title

---
+++ kubernetes/gitlab/README.md
@@ -0,0 +1,50 @@
+# Provisioning GitLab using Helm (through Terraform)
+
+This exercise shows how GitLab can be provisioned using Helm on a Kubernetes cluster. This approach allows for greater configurability compared to the traditional omnibus docker image.
+
+## Context
+
+Years ago, I would routinely provision GitLab for local development and testing using their omnibus docker image. When I had a need to provision a GitLab instance for a project recently, I found that some application settings were not configurable via the omnibus image's environment variables. After some research, I found that GitLab's helm chart appeared to offer more flexibility and configurability, including the ability to specify custom application settings.
+
+## Prerequisites
+
+- Docker Compose
+- Terraform
+
+## Deploying the cluster
+
+```bash
+docker compose up -d
+```
+
+## Accessing the cluster
+
+After deploying the cluster, a kubeconfig.yaml file is created in the
+root of the repository. This file can be used to access the cluster.
+
+```bash
+kubectl --kubeconfig kubeconfig.yaml get nodes
+```
+
+## Deploying GitLab
+
+```bash
+terraform init
+terraform apply
+```
+
+## Accessing GitLab
+
+Once GitLab has been deployed, you can access it via the following URL:
+
+```
+http://gitlab.172.17.0.1.nip.io
+```
+
+## Resetting the cluster
+
+A Makefile has been provided to simplify the process of resetting the cluster. This will kill the k3s cluster and delete all of its data.
+
+```bash
+make reset
+```
---
+++ kubernetes/gitlab/main.tf
@@ -0,0 +1,144 @@
+##
+# This example shows how to deploy GitLab into a
+# Kubernetes cluster using Terraform.
+#
+# GitLab is a software forge akin to GitHub, with support for Git
+# repositories, issue tracking, CI/CD, and more.
+##
+
+## Inputs
+
+variable "initial_root_password" {
+  type        = string
+  description = <<-EOT
+    Initial password to set for the root user in GitLab.
+
+    Changing this value after the initial deployment will have no
+    effect. You will need to manually change the root password in GitLab
+    or reset it via the GitLab Rails console.
+
+    @see https://gitlab.com/gitlab-org/charts/gitlab/-/tags
+  EOT
+}
+
+variable "gitlab_chart_version" {
+  type    = string
+  default = "8.10.3"
+}
+
+## Required Providers
+
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
+    }
+    kubectl = {
+      source  = "alekc/kubectl"
+      version = "~> 2.0"
+    }
+  }
+}
+
+## Provider Configuration
+
+provider "helm" {
+  kubernetes {
+    config_path = "${path.module}/kubeconfig.yaml"
+  }
+}
+
+provider "kubectl" {
+  config_path = "${path.module}/kubeconfig.yaml"
+}
+
+## Resources
+
+### GitLab
+
+# Ensure the Kubernetes namespace for GitLab exists
+resource "kubectl_manifest" "gitlab_namespace" {
+  yaml_body = yamlencode({
+    apiVersion = "v1"
+    kind       = "Namespace"
+    metadata = {
+      name = "gitlab"
+    }
+  })
+}
+
+resource "kubectl_manifest" "initial_root_password" {
+  yaml_body = yamlencode({
+    apiVersion = "v1"
+    kind       = "Secret"
+    metadata = {
+      name      = "gitlab-initial-root-password"
+      namespace = "gitlab"
+    }
+    type = "Opaque"
+    data = {
+      password = base64encode(var.initial_root_password)
+    }
+  })
+
+  depends_on = [
+    kubectl_manifest.gitlab_namespace
+  ]
+}
+
+resource "helm_release" "gitlab" {
+  name       = "gitlab"
+  repository = "https://charts.gitlab.io"
+  chart      = "gitlab"
+  version    = var.gitlab_chart_version
+  namespace  = "gitlab"
+
+  values = [
+    # @see https://gitlab.com/gitlab-org/charts/gitlab/-/blob/master/values.yaml
+    yamlencode({
+      global = {
+        edition = "ce"
+
+        hosts = {
+          domain = "172.17.0.1.nip.io"
+          https  = false
+        }
+
+        ingress = {
+          configureCertmanager = false
+          tls = {
+            enabled = false
+          }
+        }
+
+        initialRootPassword = {
+          secret = "gitlab-initial-root-password"
+          key    = "password"
+        }
+      }
+
+      certmanager = {
+        install = false
+      }
+
+      nginx-ingress = {
+        controller = {
+          service = {
+            type = "NodePort"
+            nodePorts = {
+              http = 30080
+            }
+            enableHttps = false
+          }
+        }
+      }
+    })
+  ]
+
+  depends_on = [
+    kubectl_manifest.initial_root_password
+  ]
+}
