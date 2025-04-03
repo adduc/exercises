4bc8f25f42a882583c9f51f775b03a0e6cf0fa07
John Long <john@johnsdomain.com>
2025-04-03 06:36:01 -0500

    laravel/auth: refactor away use of facades, login after successful registration

--- laravel/auth/app/Http/Controllers/AuthController.php
+++ laravel/auth/app/Http/Controllers/AuthController.php
@@ -5,10 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Models;
-use Illuminate\Support\Facades\{Auth, Hash};
+use Illuminate\Auth\AuthManager;
+use Illuminate\Contracts\Hashing\Hasher;
 
 class AuthController extends Controller
 {
+    public function __construct(
+        protected AuthManager $authManager,
+        protected Hasher $hasher,
+    ) {}
+
     public function showRegisterForm()
     {
         return view('auth.register');
@@ -21,14 +27,16 @@ class AuthController extends Controller
             'password' => 'required|string|min:8|max:255|confirmed',
         ]);
 
-        Models\User::create([
+        $user = Models\User::create([
             'email' => $request->email,
-            'password' => Hash::make($request->password),
+            'password' => $this->hasher->make($request->password),
         ]);
 
+        $this->authManager->login($user);
+
         return redirect()
-            ->route('login.form')
-            ->with('success', 'Registration successful. Please log in.');
+            ->route('dashboard')
+            ->with('success', 'Registration successful. Welcome!');
     }
 
     public function showLoginForm()
@@ -43,7 +51,7 @@ class AuthController extends Controller
             'password' => 'required|string',
         ]);
 
-        if (Auth::attempt($request->only('email', 'password'))) {
+        if ($this->authManager->attempt($request->only('email', 'password'))) {
             $route = route('dashboard');
             return redirect()->intended($route)
                 ->with('success', 'Logged in successfully.');
@@ -54,7 +62,7 @@ class AuthController extends Controller
 
     public function logout(Request $request)
     {
-        Auth::logout();
+        $this->authManager->logout();
 
         return redirect()->route('login.form')
             ->with('success', 'Logged out successfully.');
