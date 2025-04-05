bfedf510d7dc9ce99bd347c78d0ce683387092c1
John Long <john@johnsdomain.com>
2025-04-05 17:14:34 -0500

    gitlab/infra: initial commit

---
+++ .gitlab/infra/README.md
@@ -0,0 +1,3 @@
+# Repository Infrastructure-as-Code
+
+This directory contains Terraform code to manage this repository's configuration in GitLab, including secrets, branch protection rules, and other settings.
---
+++ .gitlab/infra/gitlab.main.tf
@@ -0,0 +1,45 @@
+##
+# GitLab Project Configuration
+##
+
+## Inputs
+
+variable "base_url" {
+  type        = string
+  description = "The base URL for the GitLab instance."
+}
+
+variable "token" {
+  type        = string
+  sensitive   = true
+  description = <<-EOT
+    The OAuth2 Token, Project, Group, Personal Access Token or CI Job
+    Token used to connect to GitLab
+  EOT
+}
+
+## Providers
+
+provider "gitlab" {
+  base_url = var.base_url
+  token    = var.token
+}
+
+## Required Providers
+
+terraform {
+  required_providers {
+    gitlab = {
+      source  = "gitlabhq/gitlab"
+      version = "17.10.0"
+    }
+  }
+}
+
+## Resources
+
+resource "gitlab_project" "project" {
+  name             = "exercises"
+  visibility_level = "public"
+  default_branch   = "main"
+}
