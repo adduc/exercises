13a3b1849acb7c11edc73a925b8297751d260c77
John Long <john@johnsdomain.com>
2025-05-10 18:55:08 -0500

    fix shellcheck violations

--- ash/sourced-script-filename/entry/entrypoint.sh
+++ ash/sourced-script-filename/entry/entrypoint.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
+# shellcheck disable=SC3040
 set -o errexit -o nounset -o pipefail
 
-SCRIPT_PATH=$(realpath $(\' 2>&1 >/dev/null | sed 's/\(.*: \)\?\(.*\): line .*/\2/' || true))
-SCRIPT_DIR=$(dirname $(realpath $(\' 2>&1 >/dev/null | sed 's/\(.*: \)\?\(.*\): line .*/\2/' || true)))
+SCRIPT_PATH=$(realpath "$(/sys/asdf 2>&1 >/dev/null | sed 's/\(.*: \)\?\(.*\): line .*/\2/' || true)")
+SCRIPT_DIR=$(dirname "$(realpath "$(/sys/asdf 2>&1 >/dev/null | sed 's/\(.*: \)\?\(.*\): line .*/\2/' || true)")")
 
 echo "SCRIPT_PATH: ${SCRIPT_PATH}"
 echo "SCRIPT_DIR: ${SCRIPT_DIR}"
 
 echo "sourcing source.sh"
-source "${SCRIPT_DIR}/../source/source.sh"
+. "${SCRIPT_DIR}/../source/source.sh"
