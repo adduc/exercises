d798e3ba7f12281048ed05621fb0027dff38eb52
John Long <john@johnsdomain.com>
2025-05-18 10:14:57 -0500

    systemd/on-failure: initial commit

---
+++ systemd/on-failure/services/OneShot.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Starting service..."
+echo "Timestamp: $(date)"
+echo "PID: $$"
+
+env
+
+# for 1 to 5
+for i in {1..2}; do
+  echo "Example output: $RANDOM"
+  sleep 1
+done
+
+echo "Exiting with an error"
+exit 1
