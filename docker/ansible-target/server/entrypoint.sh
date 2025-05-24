3aae9b70b5b4ac57b7c909520d4ae4f729335cc0
John Long <john@johnsdomain.com>
2025-05-24 11:44:02 -0500

    Fix shellcheck violations

--- docker/ansible-target/server/entrypoint.sh
+++ docker/ansible-target/server/entrypoint.sh
@@ -8,7 +8,7 @@ set -o nounset -o errexit -o pipefail
 create_group() {
   echo "Checking if group ${USER} exists..."
 
-  if getent group ${USER} > /dev/null; then
+  if getent group "${USER}" > /dev/null; then
     echo "Group ${USER} already exists. Skipping group creation."
     return 0
   fi
@@ -16,8 +16,8 @@ create_group() {
   echo "Creating group ${USER} with GID ${GID}..."
 
   groupadd \
-    --gid ${GID} \
-    ${USER}
+    --gid "${GID}" \
+    "${USER}"
 
   echo "Group ${USER} created successfully."
 }
@@ -25,7 +25,7 @@ create_group() {
 create_user() {
   echo "Checking if user ${USER} exists..."
 
-  if getent passwd ${USER} > /dev/null; then
+  if getent passwd "${USER}" > /dev/null; then
       echo "User ${USER} already exists. Skipping user creation."
       return 0
   fi
@@ -34,10 +34,10 @@ create_user() {
 
   useradd \
     --create-home \
-    --gid ${GID} \
+    --gid "${GID}" \
     --shell /bin/bash \
-    --uid ${UID} \
-    ${USER}
+    --uid "${UID}" \
+    "${USER}"
 
     echo "User ${USER} created successfully."
 }
@@ -45,10 +45,10 @@ create_user() {
 prepare_user_ssh() {
   [ -d /mnt/user-ssh ] || return 0
   echo "Preparing SSH configuration for user ${USER}..."
-  mkdir -p /home/${USER}/.ssh
+  mkdir -p /home/"${USER}"/.ssh
 
   echo "Copying SSH keys from /mnt/user-ssh to /home/${USER}/.ssh..."
-  cp -r /mnt/user-ssh/* /home/${USER}/.ssh/
+  cp -r /mnt/user-ssh/* /home/"${USER}"/.ssh/
 
   echo "Trusting localhost SSH key..."
 
@@ -59,20 +59,20 @@ prepare_system_ssh() {
 
   echo "Trusting system SSH keys..."
 
-  truncate -s 0 /home/${USER}/.ssh/known_hosts
+  truncate -s 0 /home/"${USER}"/.ssh/known_hosts
 
   for file in /mnt/system-ssh/ssh_host_*.pub; do
     # use wildcard to trust fingerprint for all hostnames
-    (echo -n "* "; cat "$file") >> /home/${USER}/.ssh/known_hosts
+    (echo -n "* "; cat "$file") >> /home/"${USER}"/.ssh/known_hosts
   done
 }
 
 set_ssh_ownership() {
   echo "Setting ownership of SSH directories and files..."
 
-  chown -R ${USER}:${USER} /home/${USER}/.ssh
-  find /home/${USER}/.ssh -type d -exec chmod 700 {} +
-  find /home/${USER}/.ssh -type f -exec chmod 600 {} +
+  chown -R "${USER}:${USER}" /home/"${USER}"/.ssh
+  find /home/"${USER}"/.ssh -type d -exec chmod 700 {} +
+  find /home/"${USER}"/.ssh -type f -exec chmod 600 {} +
 }
 
 exec_cmd() {
--- docker/ansible-target/target/entrypoint.sh
+++ docker/ansible-target/target/entrypoint.sh
@@ -8,7 +8,7 @@ set -o nounset -o errexit -o pipefail
 create_group() {
   echo "Checking if group ${USER} exists..."
 
-  if getent group ${USER} > /dev/null; then
+  if getent group "${USER}" > /dev/null; then
     echo "Group ${USER} already exists. Skipping group creation."
     return 0
   fi
@@ -16,8 +16,8 @@ create_group() {
   echo "Creating group ${USER} with GID ${GID}..."
 
   groupadd \
-    --gid ${GID} \
-    ${USER}
+    --gid "${GID}" \
+    "${USER}"
 
   echo "Group ${USER} created successfully."
 }
@@ -25,7 +25,7 @@ create_group() {
 create_user() {
   echo "Checking if user ${USER} exists..."
 
-  if getent passwd ${USER} > /dev/null; then
+  if getent passwd "${USER}" > /dev/null; then
       echo "User ${USER} already exists. Skipping user creation."
       return 0
   fi
@@ -34,38 +34,38 @@ create_user() {
 
   useradd \
     --create-home \
-    --gid ${GID} \
+    --gid "${GID}" \
     --shell /bin/bash \
-    --uid ${UID} \
-    ${USER}
+    --uid "${UID}" \
+    "${USER}"
 }
 
 grant_sudo_access() {
     echo "Checking if sudo access is already granted to ${USER}..."
 
-    if sudo -l -U ${USER} | grep -q '(ALL : ALL) ALL'; then
+    if sudo -l -U "${USER}" | grep -q '(ALL : ALL) ALL'; then
         echo "Sudo access already granted to ${USER}. Skipping sudo configuration."
         return 0
     fi
 
     echo "Granting sudo access to ${USER}..."
 
-    echo "${USER} ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/${USER}
-    chmod 0440 /etc/sudoers.d/${USER}
-    chown root:root /etc/sudoers.d/${USER}
+    echo "${USER} ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/"${USER}"
+    chmod 0440 /etc/sudoers.d/"${USER}"
+    chown root:root /etc/sudoers.d/"${USER}"
 }
 
 prepare_user_ssh() {
   [ -d /mnt/user-ssh ] || return 0
 
   echo "Preparing SSH configuration for user ${USER}..."
-  mkdir -p /home/${USER}/.ssh
+  mkdir -p /home/"${USER}"/.ssh
 
   echo "Copying SSH keys from /mnt/user-ssh to /home/${USER}/.ssh..."
-  cp -r /mnt/user-ssh/* /home/${USER}/.ssh/
+  cp -r /mnt/user-ssh/* /home/"${USER}"/.ssh/
 
   echo "Allowing SSH keys in /home/${USER}/.ssh to be used for authentication..."
-  cat /home/${USER}/.ssh/*.pub > /home/${USER}/.ssh/authorized_keys
+  cat /home/"${USER}"/.ssh/*.pub > /home/"${USER}"/.ssh/authorized_keys
 }
 
 prepare_system_ssh() {
@@ -79,9 +79,9 @@ prepare_system_ssh() {
 set_ssh_ownership() {
   echo "Setting ownership of SSH directories and files..."
 
-  chown -R ${USER}:${USER} /home/${USER}/.ssh
-  find /home/${USER}/.ssh -type d -exec chmod 700 {} +
-  find /home/${USER}/.ssh -type f -exec chmod 600 {} +
+  chown -R "${USER}:${USER}" /home/"${USER}"/.ssh
+  find /home/"${USER}"/.ssh -type d -exec chmod 700 {} +
+  find /home/"${USER}"/.ssh -type f -exec chmod 600 {} +
 }
 
 exec_cmd() {
